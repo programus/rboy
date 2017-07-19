73515d83e445eed6dcbaa97c3d1b4a7217a30180
programus <programus@gmail.com>
2017-07-19 20:49:45 +0900

    added speed indicators

--- src/rboy/src/main.ino
+++ src/rboy/src/main.ino
@@ -17,6 +17,7 @@
 #define FONTH       8
 
 #define LIMIT       3000L
+#define SPD_UNIT    70
 
 const long MAX_NUM = MAX_DISP_NUM * LIMIT;
 
@@ -39,23 +40,49 @@ void display_log(const char* message, bool complete=true, uint8_t size=1) {
   }
 }
 
-void show_number(int n, uint8_t size=5) {
-  display.clearDisplay();
-  display.setTextSize(size);
-  display.setTextColor(WHITE);
+void show_number(int n, long speed=0, uint8_t size=5) {
   uint16_t fw = FONTW * size;
   uint16_t fh = FONTH * size;
   uint16_t x = (LCDW >> 1) - fw;
   uint16_t y = (LCDH - fh) >> 1;
   uint16_t mx = 4;
   uint16_t my = 2;
-  for (int i = 0; i < 2; i++) {
+  uint8_t frame_count = 2;
+  // draw frames
+  for (uint8_t i = 0; i < frame_count; i++) {
     int offset = i << 1;
     display.drawRect(x - mx - 1 - offset, y - my - 1 - offset, (fw << 1) + (mx << 1) + (offset << 1), fh + (my << 1) + (offset << 1), WHITE);
   }
+
+  // draw speed indicators
+  uint8_t sx[2];
+  sx[0] = (x - mx - 1 - ((frame_count) << 1)) >> 1;
+  sx[1] = LCDW - sx[0];
+  uint8_t sy = LCDH >> 1;
+
+  uint8_t rr = sx[0] >> 1;
+  for (uint8_t i = 0; i < sizeof(sx); i++) {
+    int8_t ssx = sx[i];
+    int8_t padding = 3;
+    int8_t space = 4;
+    display.fillCircle(ssx, sy, rr >> 2, WHITE);
+    for (uint8_t j = 1; j < abs(speed) + 1; j++) {
+      int8_t offset = j * space + padding;
+      int8_t ssy = sy + (speed > 0 ? -offset : offset);
+      int8_t d = rr >> 1;
+      int8_t dx = d;
+      int8_t dy = speed > 0 ? d : -d;
+      display.drawLine(ssx, ssy, ssx - dx, ssy + dy, WHITE);
+      display.drawLine(ssx, ssy, ssx + dx, ssy + dy, WHITE);
+    }
+  }
+
+  // draw number
   if (n < 10) {
     x += fw;
   }
+  display.setTextSize(size);
+  display.setTextColor(WHITE);
   display.setCursor(x, y);
   display.print(n);
 }
@@ -106,16 +133,18 @@ void loop() {
   unsigned long time = millis();
   long dt = time - prev;
   prev = time;
-  number += delta * dt;
+  long speed = delta * dt;
+  number += speed;
   if (number <= 0) {
     number = MAX_NUM - 1;
   }
   number = number % MAX_NUM;
-  display.invertDisplay(delta < 0);
-  show_number(number / LIMIT);
+  display.clearDisplay();
+  show_number(number / LIMIT, speed / SPD_UNIT);
 
   // display.setCursor(0, 0);
   // display.setTextSize(1);
+  // display.print(speed / SPD_UNIT);
 
   display.display();
 }
